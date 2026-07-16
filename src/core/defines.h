e1f8858a8d96b9d7aa521849185da7b8bf49d5e8
wwhasmile <uwuhasmile@proton.me>
2026-07-16 16:55:14 +0300

    Added defines header

---
+++ src/core/defines.h
@@ -0,0 +1,20 @@
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef u8 bool;
+#define TRUE 1U
+#define FALSE 0U
+
+#define NULL ((void*)0)
+
+#endif // __DEFINES_H__
